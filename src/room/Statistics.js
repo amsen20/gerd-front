f564dd03d775a07c865ba122243adccf191cf7ce
amsen20 <amsen@aut.ac.ir>
2022-03-20 23:27:03 +0330

    Add statistics.

--- src/room/Statistics.js
+++ src/room/Statistics.js
@@ -1,4 +1,30 @@
+import {useState} from "react";
+
 export default function Statistics({room}) {
-    const isMatchStarted = !!room.match;
+    let [RemainingTime, setRemainingTime] = useState(0);
+
+    if(!room.match)
+        return <></>;
+
+    const {current_round, team_one_score, team_two_score, round_start_time, total_round_time} = room.match;
+
+    setInterval(() => {
+        setRemainingTime(total_round_time - Math.round((Date.now() - Date.parse(round_start_time))/1000));
+    }, 1000);
+
+    return <div>
+        <p>
+            team one: {team_one_score}
+        </p>
+        <p>
+            team two: {team_two_score}
+        </p>
+        <p>
+            current round: {current_round}
+        </p>
+        <p>
+            current time: {RemainingTime}
+        </p>
+    </div>
 
 }
